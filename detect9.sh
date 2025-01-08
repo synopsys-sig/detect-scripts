4d7c52c58570e9b67e013b3ee6ef2ac87b5a1d7d
Andrian Sevastyanov <andrians@blackduck.com>
2025-01-08 13:45:37 -0700

    Updating wording based on review suggestions

--- detect9.sh
+++ detect9.sh
@@ -3,18 +3,18 @@
 write_rebranding_msg() {
 	echo "***"
 	echo "***"
-	echo "*** Warning: Synopsys Detect has been renamed and rebranded as part of Black Duck being established as a standalone company."
-	echo "*** Please modify your processes to download Black Duck Detect from https://detect.blackduck.com/detect9.sh before 14th February 2025."
-	echo "*** From 14th February 2025 this script will fail and not execute any scans."
-	echo "*** For more information please see the domain name change annoucement here: https://community.blackduck.com/s/question/0D5Uh00000JsImfKAF/black-duck-detect-scripts-and-the-impact-of-decommissioning-of-detectsynopsyscom"
+	echo "*** Warning: Synopsys Detect has been rebranded as Black Duck Detect."
+	echo "*** Please modify your processes to download Black Duck Detect from https://detect.blackduck.com/detect9.sh before February 14th, 2025, to prevent script and scan execution failure."
+	echo "*** For more information, please see the domain name change annoucement here: https://community.blackduck.com/s/question/0D5Uh00000JsImfKAF/black-duck-detect-scripts-and-the-impact-of-decommissioning-of-detectsynopsyscom"
 	echo "***"
-	echo "*** If you are seeing this warning, and using Synopsys Detect Azure extension, migrate to Black Duck Detect Azure extension before Feb 14th."
-	echo "*** For more information please see the extension release announcement here: https://community.blackduck.com/s/question/0D5Uh00000Mz1AcKAJ/black-duck-detect-azure-devops-ado-plugin-1000-has-been-released"
+	echo "*** If you are using the Synopsys Detect Azure extension, migrate to the Black Duck Detect Azure extension before February 14th, 2025."
+	echo "*** For more information, please see the extension release announcement here: https://community.blackduck.com/s/question/0D5Uh00000Mz1AcKAJ/black-duck-detect-azure-devops-ado-plugin-1000-has-been-released"
 	echo "***"
-	echo "*** If you are seeing this warning, and using Synopsys Detect Jenkins plugin (9.0.0 or prior), upgrade/migrate to Black Duck Detect Jenkins Plugin 10.0.0 before Feb 14th."
-	echo "*** For more information please see the plugin release announcement here: https://community.blackduck.com/s/question/0D5Uh00000Mtso3KAB/black-duck-detect-jenkins-plugin-1000-has-been-released"
+	echo "*** If you are using Synopsys Detect Jenkins plugin (9.0.0 or prior), migrate to Black Duck Detect Jenkins Plugin 10.0.0 before February 14th, 2025."
+	echo "*** For more information, please see the plugin release announcement here: https://community.blackduck.com/s/question/0D5Uh00000Mtso3KAB/black-duck-detect-jenkins-plugin-1000-has-been-released"
 	echo "***"
 	echo "***"
+
 }
 
 write_rebranding_msg
